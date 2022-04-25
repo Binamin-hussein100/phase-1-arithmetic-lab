d3b61d6cb2f53dadf554bded91ba142b95a3c3f4
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-04-25 17:07:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 1
+const num2 = 62
+const num3 = 4
+const num4 = 20
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()*10)
+
+const mod = num3 % num4
+
+const max = Math.max(1,2,3,4,5,20)
